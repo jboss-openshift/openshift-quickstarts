f724c2ea57633717be4c6fe862f131756f4439c6
Marko Luksa <marko.luksa@gmail.com>
2015-09-16 22:58:47 +0200

    Handle Tomcat's JNDI specifics

--- todolist/todolist-jdbc/src/main/java/org/openshift/quickstarts/todolist/dao/JdbcTodoListDAO.java
+++ todolist/todolist-jdbc/src/main/java/org/openshift/quickstarts/todolist/dao/JdbcTodoListDAO.java
@@ -4,6 +4,7 @@ import org.openshift.quickstarts.todolist.model.TodoEntry;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
+import javax.naming.NameNotFoundException;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
 import java.sql.*;
@@ -27,7 +28,12 @@ public class JdbcTodoListDAO implements TodoListDAO {
     private DataSource lookupDataSource() {
         try {
             Context initialContext = new InitialContext();
-            return (DataSource) initialContext.lookup(System.getenv("DB_JNDI"));
+            try {
+                return (DataSource) initialContext.lookup(System.getenv("DB_JNDI"));
+            } catch (NameNotFoundException e) {
+                Context envContext = (Context) initialContext.lookup("java:comp/env");  // Tomcat places datasources inside java:comp/env
+                return (DataSource) envContext.lookup(System.getenv("DB_JNDI"));
+            }
         } catch (NamingException e) {
             throw new RuntimeException("Could not look up datasource", e);
         }
